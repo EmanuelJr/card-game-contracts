d1d408b5b99fd28215f18b1657c1ac95e62cbdb1
Emanuel Júnior <emanuelmpjr@gmail.com>
2023-09-27 20:47:22 -0300

    feat: migrate to erc1155

--- src/CardGame.sol
+++ src/CardGame.sol
@@ -1,13 +1,16 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CardGame is ERC721, ERC721Burnable, Ownable {
+contract CardGame is ERC1155, ERC1155Burnable, AccessControl, Pausable {
     using Counters for Counters.Counter;
+
+    bytes32 public constant PRINTER_ROLE = keccak256("PRINTER_ROLE");
 
     enum CardRarity {
         COMMON,
@@ -19,7 +22,6 @@ contract CardGame is ERC721, ERC721Burnable, Ownable {
     struct Set {
         string name;
         string code;
-        bool legal;
     }
 
     struct Card {
@@ -30,44 +32,57 @@ contract CardGame is ERC721, ERC721Burnable, Ownable {
         bool banned;
     }
 
-    Counters.Counter private _tokenIdCounter;
-    Counters.Counter private _cardIdCounter;
     Counters.Counter private _setIdCounter;
-    string public baseURI;
+    Counters.Counter private _cardIdCounter;
 
     mapping(uint256 => Set) public sets;
     mapping(uint256 => Card) public cards;
-    mapping(uint256 => uint256) public prints; // tokenId => cardId
 
-    event PrintMinted(uint256 indexed tokenId, uint256 indexed cardId);
     event SetCreated(uint256 indexed setId, string name);
     event CardCreated(uint256 indexed cardId, string name);
     event CardBanned(uint256 indexed cardId);
 
-    constructor(string memory initialBaseURI) ERC721("Card Game", "TCG") {
-        baseURI = initialBaseURI;
-    }
+    constructor(string memory initialBaseURI) ERC1155(initialBaseURI) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PRINTER_ROLE, msg.sender);
 
-    function printCard(address to, uint256 cardId) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        prints[tokenId] = cardId;
-
-        emit PrintMinted(tokenId, cardId);
-    }
-
-    function createSet(
-        string memory name,
-        string memory code,
-        bool legal
-    ) public onlyOwner returns (uint256) {
-        uint256 setId = _setIdCounter.current();
         _setIdCounter.increment();
-        sets[setId] = Set(name, code, legal);
+        _cardIdCounter.increment();
+    }
 
-        emit SetCreated(setId, name);
-        return setId;
+    // Print functions
+    function print(
+        address to,
+        uint256 cardId,
+        uint256 amount,
+        bytes memory data
+    ) public onlyRole(PRINTER_ROLE) {
+        _mint(to, cardId, amount, data);
+    }
+
+    function printBatch(
+        address to,
+        uint256[] memory cardIds,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyRole(PRINTER_ROLE) {
+        _mintBatch(to, cardIds, amounts, data);
+    }
+
+    function getAccountPrintedCards(
+        address wallet
+    ) public view returns (uint256[] memory) {
+        uint256[] memory balances = new uint256[](_cardIdCounter.current() - 1);
+        uint256 indexCounter = 0;
+
+        for (uint256 i = 1; i < _cardIdCounter.current(); i++) {
+            if (balanceOf(wallet, i) > 0) {
+                balances[indexCounter] = i;
+                indexCounter++;
+            }
+        }
+
+        return balances;
     }
 
     // Card functions
@@ -76,7 +91,7 @@ contract CardGame is ERC721, ERC721Burnable, Ownable {
         string memory description,
         CardRarity rarity,
         uint256 setId
-    ) public onlyOwner returns (uint256) {
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
         uint256 cardId = _cardIdCounter.current();
         _cardIdCounter.increment();
         cards[cardId] = Card(name, description, rarity, setId, false);
@@ -89,40 +104,56 @@ contract CardGame is ERC721, ERC721Burnable, Ownable {
         return cards[cardId];
     }
 
-    function banCard(uint256 cardId) public onlyOwner {
+    function banCard(uint256 cardId) public onlyRole(DEFAULT_ADMIN_ROLE) {
         cards[cardId].banned = true;
 
         emit CardBanned(cardId);
     }
 
     // Set functions
+    function createSet(
+        string memory name,
+        string memory code
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
+        uint256 setId = _setIdCounter.current();
+        _setIdCounter.increment();
+        sets[setId] = Set(name, code);
+
+        emit SetCreated(setId, name);
+        return setId;
+    }
+
     function getSet(uint256 setId) public view returns (Set memory) {
         return sets[setId];
     }
 
-    // Printed cards functions
-    function getCardPrinted(uint256 tokenId) public view returns (Card memory) {
-        return cards[prints[tokenId]];
+    // Contract itself related functions
+    function togglePause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (paused()) {
+            _unpause();
+        } else {
+            _pause();
+        }
     }
 
-    // ERC721 related functions
-    function setBaseURI(string memory uri) public onlyOwner {
-        _setBaseURI(uri);
+    function setURI(string memory newuri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setURI(newuri);
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override returns (string memory) {
-        super._exists(tokenId);
-        uint256 cardId = prints[tokenId];
-        return string(abi.encodePacked(_baseURI(), cardId));
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
-
-    function _setBaseURI(string memory uri) internal {
-        baseURI = uri;
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
